fd0b4517baf978e22fe5cd3952c58d556e51cf48
evanwporter <evanwporter@gmail.com>
2024-11-02 14:01:47 -0700

    Fixed Bugs
    * Fixed Marking bug in Gen games with multicolored markings
    * Added egg location index to PK4

--- src/pkm/OHPKM.ts
+++ src/pkm/OHPKM.ts
@@ -252,12 +252,12 @@ export class OHPKM {
       this.isNoble = other.isNoble ?? false
       this.isShadow = other.isShadow ?? false
       this.markings = types.markingsSixShapesWithColorFromOther(other.markings) ?? {
-        circle: false,
-        triangle: false,
-        square: false,
-        heart: false,
-        star: false,
-        diamond: false,
+        circle: null,
+        triangle: null,
+        square: null,
+        heart: null,
+        star: null,
+        diamond: null,
       }
       this.alphaMove = other.alphaMove ?? 0
       this.personalityValue = other.personalityValue ?? 0
