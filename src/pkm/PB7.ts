f6944f6143aac872819c74183052341006372aa8
evanwporter <evanwporter@gmail.com>
2024-11-06 19:16:16 -0800

    Undid a couple files

--- src/pkm/PB7.ts
+++ src/pkm/PB7.ts
@@ -165,12 +165,12 @@ export class PB7 {
       this.abilityNum = other.abilityNum ?? 0
       this.favorite = other.favorite ?? false
       this.markings = types.markingsSixShapesWithColorFromOther(other.markings) ?? {
-        circle: null,
-        triangle: null,
-        square: null,
-        heart: null,
-        star: null,
-        diamond: null,
+        circle: false,
+        triangle: false,
+        square: false,
+        heart: false,
+        star: false,
+        diamond: false,
       }
       this.personalityValue = other.personalityValue ?? 0
       this.nature = other.nature ?? 0
